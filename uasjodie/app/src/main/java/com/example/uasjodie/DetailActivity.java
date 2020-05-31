eadff9dc7571362251ce694b2e17ddcf190875e3
jodiebonanza <66155785+jodiebonanza@users.noreply.github.com>
2020-05-31 22:21:57 +0800

    Update DetailActivity.java

--- uasjodie/app/src/main/java/com/example/uasjodie/DetailActivity.java
+++ uasjodie/app/src/main/java/com/example/uasjodie/DetailActivity.java
@@ -2,7 +2,16 @@ package com.example.uasjodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import static com.example.uasjodie.MainActivity.EXTRA_IMG;
+import static com.example.uasjodie.MainActivity.EXTRA_JABATAN;
+import static com.example.uasjodie.MainActivity.EXTRA_NAMA;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +19,22 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String gambar = intent.getStringExtra(EXTRA_IMG);
+        String nama = intent.getStringExtra(EXTRA_NAMA);
+        String jabatan = intent.getStringExtra(EXTRA_JABATAN);
+
+       ImageView imgambar = findViewById(R.id.img_detail);
+        TextView tvnama = findViewById(R.id.nama_det);
+        TextView tvharga = findViewById(R.id.jabatan_detail);
+
+        Glide
+                .with(this)
+                .load(gambar)
+                .into(imgambar);
+
+        tvnama.setText(nama);
+        tvharga.setText(jabatan);
     }
 }
